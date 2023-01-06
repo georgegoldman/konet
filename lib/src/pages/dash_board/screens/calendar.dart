aa4d857a1e3954c07bda505a19832718d94cefb0
georgegoldman <georgegoldmanjohn.o.com>
2023-01-06 12:34:32 +0100

    calendar updates

--- lib/src/pages/dash_board/screens/calendar.dart
+++ lib/src/pages/dash_board/screens/calendar.dart
@@ -107,31 +107,24 @@ class _CalendarState extends State<Calendar> {
         const SizedBox(
           height: 8,
         ),
-        Expanded(
-          child: ValueListenableBuilder<List<Event>>(
-            valueListenable: _selectedEvents,
-            builder: (context, value, _) {
-              return ListView.builder(
-                itemCount: value.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 12.0,
-                      vertical: 4.0,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: ListTile(
-                      onTap: () => print('${value[index]}'),
-                      title: Text('${value[index]}'),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
+        ValueListenableBuilder<List<Event>>(
+          valueListenable: _selectedEvents,
+          builder: (context, value, _) {
+            return ListView.builder(
+              itemCount: value.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 3),
+                  child: ListTile(
+                    leading: const Icon(Icons.person),
+                    title: Text('${value[index]}'),
+                    tileColor: Colors.grey[300],
+                    onTap: () => print('${value[index]}'),
+                  ),
+                );
+              },
+            );
+          },
         ),
       ],
     );
