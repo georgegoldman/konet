8a91c17e82d5612890264f09dc8263f8e7a086bc
georgegoldman <georgegoldmanjohn.o.com>
2023-01-09 18:20:23 +0100

    upt

--- lib/src/pages/dash_board/screens/service.dart
+++ lib/src/pages/dash_board/screens/service.dart
@@ -16,45 +16,43 @@ class _ServiceState extends State<Service> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black12,
-          elevation: 4,
-          bottom: TabBar(
-              unselectedLabelColor: Colors.black,
-              indicatorSize: TabBarIndicatorSize.tab,
-              indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: const Color(0xFFE6B325)),
-              tabs: [
-                Tab(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: TextButton.icon(
+          bottom: PreferredSize(
+            preferredSize:
+                Size.fromHeight(MediaQuery.of(context).size.height * 0.03),
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.05),
+              child: TabBar(
+                  unselectedLabelColor: Colors.black,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: const Color(0xFFE6B325)),
+                  tabs: [
+                    const Tab(
+                      icon: Icon(Icons.edit_off),
+                      text: 'service',
+                    ),
+                    Tab(
+                        child: TextButton.icon(
+                      onPressed: null,
+                      icon: const Icon(Icons.layers_outlined, size: 15),
+                      label: const Text('Bundle'),
+                    )),
+                    Tab(
+                      child: TextButton.icon(
                         onPressed: null,
                         icon: const Icon(
-                          Icons.edit_off,
+                          Icons.add_to_photos_rounded,
                           size: 15,
                         ),
-                        label: const Text('service')),
-                  ),
-                  // icon: Icon(Icons.edit_off),
-                  // text: 'service',
-                ),
-                Tab(
-                    child: TextButton.icon(
-                  onPressed: null,
-                  icon: const Icon(Icons.layers_outlined, size: 15),
-                  label: const Text('Bundle'),
-                )),
-                Tab(
-                  child: TextButton.icon(
-                    onPressed: null,
-                    icon: const Icon(
-                      Icons.add_to_photos_rounded,
-                      size: 15,
+                        label: const Text('addons'),
+                      ),
                     ),
-                    label: const Text('addons'),
-                  ),
-                ),
-              ]),
+                  ]),
+            ),
+          ),
+          elevation: 0,
         ),
         body: const TabBarView(children: [
           Icon(Icons.directions_car),
